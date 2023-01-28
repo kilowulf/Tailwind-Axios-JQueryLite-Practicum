249d4f2c0b920e588252a829530d0b8439ad44e7
kilowulf <kilonow1275@hotmail.com>
2023-01-27 18:56:05 -0700

    cleaned up some useless comments

--- public/script.js
+++ public/script.js
@@ -1,44 +1,4 @@
-/* Disable Add Log button until logs are displayed */
 
-// let $ = (sel) => {
-//   let els = [...document.querySelectorAll(sel)];
-
-//   return {
-//     css: (propName, value) => {
-//       els.forEach((el) => (el.style[propName] = value));
-//     },
-
-//     toggle: () => {
-//       alert('toggle');
-
-//       els.forEach((el) => el); //does nothing; placeholder
-
-//       //TODO if showing, hide
-
-//       //TODO if hidden, show
-//     },
-
-//     click: (callBackFn) => {
-//       els.forEach(callBackFn); //does nothing; placeholder
-
-//       //TODO assign the click event the selected elements
-//     },
-//   };
-// };
-
-// function exampleUsage() {
-//   $('h1').css('color', 'red');
-
-//   $('h2').css('color', 'green');
-
-//   $('#second').toggle();
-
-//   $('div').toggle();
-
-//   $('.log').click(() => alert('click')); //alert callback is a form of stubbing
-// }
-
-// exampleUsage();
 
 $(document).ready(function () {
   // Disable Add Log button until logs are displayed
